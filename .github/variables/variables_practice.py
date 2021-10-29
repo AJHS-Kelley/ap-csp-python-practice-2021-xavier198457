a3ab1816a35bc0ceaef66b751ccd2a661386b9a2
xavier198457 <90337171+xavier198457@users.noreply.github.com>
2021-10-29 13:02:47 -0400

    version 0.1
    Added float, integer, and string variables.

---
+++ .github/variables/variables_practice.py
@@ -0,0 +1,11 @@
+# Variables Practice, Xavier Pennington, 10/29/21 1:01pm, version 0.1
+
+
+# Basic Variables
+
+my_int = -15 # INTEGERS, any +/- number and 0
+my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal
+
+my_string = "How much wood could a woodchuck chuck if a woodchuck could chuck wood?"
+
+
