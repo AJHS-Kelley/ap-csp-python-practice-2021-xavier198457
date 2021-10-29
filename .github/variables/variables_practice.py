f3ae2047402fac46a6b45c4c1df27820f73178a2
xavier198457 <90337171+xavier198457@users.noreply.github.com>
2021-10-29 13:09:27 -0400

    version 0.2
    Added commands to print the integer, float, and string variables.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,11 +1,12 @@
-# Variables Practice, Xavier Pennington, 10/29/21 1:01pm, version 0.1
+# Variables Practice, Xavier Pennington, 10/29/21 1:08pm, version 0.2
 
 
 # Basic Variables
 
 my_int = -15 # INTEGERS, any +/- number and 0
 my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal
-
 my_string = "How much wood could a woodchuck chuck if a woodchuck could chuck wood?"
 
-
+print(my_int)
+print(my_float)
+print(my_string)
